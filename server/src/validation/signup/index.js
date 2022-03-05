b20a31754da5ac10693d0a95fafe1a96b13172e1
caio frias <caio.sousa@extreme.digital>
2022-03-05 10:32:23 -0300

    refactor: change validation method

--- server/src/validation/signup/index.js
+++ server/src/validation/signup/index.js
@@ -1,14 +1,23 @@
 module.exports = class SignUpValidator {
     handle(data) {
+        let obligatoryFields = ['account', 'password']
+
+        let errors = []
+
         if (!data) {
             return { statusCode: 400, message: 'There is no data!' }
         }
-        if (!data.account) {
-            return { statusCode: 400, message: 'The account is required!' }
+
+        for (const field of obligatoryFields) {
+            if (!data[field]) {
+                errors.push(`The field ${field} is required`)
+            }
         }
-        if (!data.password) {
-            return { statusCode: 400, message: 'The password is required!' }
+
+        if (errors.length > 0) {
+            return { statusCode: 400, message: errors.join(',') }
         }
+
         return { statusCode: 200 }
     }
 }
