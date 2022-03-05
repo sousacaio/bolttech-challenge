1a78b16fdf39f59f33d633393321d5fb6ad6281e
caio frias <caio.sousa@extreme.digital>
2022-03-05 11:25:18 -0300

    feat: add mongoose to the root of the project

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const app = require("./app");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bolttech');
 
 app.listen(3000, () => {
     console.log("App listening on port 3000!");
