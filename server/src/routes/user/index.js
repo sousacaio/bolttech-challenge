6c83e394c6cf6f395d5bb7a1d1c0b700c2f04216
caio frias <caio.sousa@extreme.digital>
2022-03-05 17:37:02 -0300

    feat: add factory to signin route

--- server/src/routes/user/index.js
+++ server/src/routes/user/index.js
@@ -1,8 +1,13 @@
 const routes = require('express').Router()
 const { makeSignupController } = require('../../infraestructure/factories/signup-factory.factory')
+const { makeLoginController } = require('../../infraestructure/factories/login-factory.factory')
 
 routes.post('/signup', async (req, res) => {
     let { statusCode, body } = await makeSignupController(req, res)
+    return res.status(statusCode).json(body)
+})
+routes.post('/signin', async (req, res) => {
+    let { statusCode, body } = await makeLoginController(req, res)
     return res.status(statusCode).json(body)
 })
 
