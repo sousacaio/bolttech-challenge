1c870d3445debafdc73760c9670344aa501909b9
caio frias <caio.sousa@extreme.digital>
2022-03-05 18:20:50 -0300

    feat: add response interceptor

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -3,4 +3,13 @@ import { Settings } from './settings'
 
 axios.defaults.baseURL = Settings.backend_endpoint
 
+axios.interceptors.response.use(
+    (response) => { return response },
+    async (error) => {
+        if (error?.response?.status === 400) {
+            alert(error.response.data.message)            
+        }        
+    }
+);
+
 export default axios
