f76201f32eada3809dbe18a9041938c2248a34a6
caio frias <caio.sousa@extreme.digital>
2022-03-05 11:33:23 -0300

    test: make sure that valid data are returned if validation works

--- server/src/interfaces/controllers/signup.controller.js
+++ server/src/interfaces/controllers/signup.controller.js
@@ -1,17 +1,26 @@
-const { badRequest } = require('../helpers/http-helpers')
+const { badRequest, ok } = require('../helpers/http-helpers')
 module.exports = class SignUpController {
 
     #validator
-    constructor(validator) {
+    #userRepository
+
+    constructor(validator, userRepository) {
         this.#validator = validator
+        this.#userRepository = userRepository
     }
 
-    handle(req, res) {
+    async handle(req, res) {
 
         const validation = this.#validator.handle(req.body)
 
         if (validation.statusCode !== 200) {
             return badRequest({ message: validation.message })
         }
+
+        const user = await this.#userRepository.save({
+            account: req.body.account,
+            password: req.body.password
+        })
+        return ok(user)
     }
 }
--- server/tests/unit/signup-controller.test.js
+++ server/tests/unit/signup-controller.test.js
@@ -1,20 +1,36 @@
+const UserRepository = require("../../src/infraestructure/repositories/user.repository")
 const SignUpController = require("../../src/interfaces/controllers/signup.controller")
 const SignUpValidator = require("../../src/validation/signup")
 describe('signup-validator', () => {
-
+    class FakeDbProvider {
+        async save({ account, password }) {
+            return Promise.resolve({ account, password, _id: 'any_id' })
+        }
+    }
+    const fakeDbProvider = new FakeDbProvider()
+    const userRepository = new UserRepository(fakeDbProvider)
     const validator = new SignUpValidator()
-    const sut = new SignUpController(validator)
+    const sut = new SignUpController(validator, userRepository)
 
-    it('Should return 400 if no valid data is provided', () => {
+    it('Should return 400 if no valid data is provided', async () => {
         let req = { body: {} }
         let res = null
-        const { statusCode } = sut.handle(req, res)
+        const { statusCode } = await sut.handle(req, res)
         expect(statusCode).toBe(400)
     })
-    it('If valid data is provided, should save user', () => {
-        let req = { body: {} }
+
+    it('If valid data is provided, should signup user', async () => {
+        let req = {
+            body: {
+                account: 'test',
+                password: 'test@test.com'
+            }
+        }
         let res = null
-        const { statusCode } = sut.handle(req, res)
-        expect(statusCode).toBe(400)
+        const { statusCode, body } = await sut.handle(req, res)
+        expect(statusCode).toBe(200)
+        expect(body.account).toBe(req.body.account)
+        expect(body.password).toBe(req.body.password)
+        expect(body._id).toBe('any_id')
     })
 })
