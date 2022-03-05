6d9d2abac597510d3b14169bdeda178fd8d697ca
caio frias <caio.sousa@extreme.digital>
2022-03-05 17:31:01 -0300

    test: ensure controller works

--- server/tests/unit/login-controller.test.js
+++ server/tests/unit/login-controller.test.js
@@ -1,38 +1,76 @@
+const UserRepository = require("../../src/infraestructure/repositories/user.repository")
+const LoginController = require("../../src/interfaces/controllers/login.controller")
 const LoginValidator = require("../../src/validation/login")
+describe('signup-validator', () => {
+    class FakeDbProvider {
+        async save({ account, password }) {
+            return Promise.resolve({ account, password, _id: 'any_id' })
+        }
+        async findByAccount(account) {
 
+            if (account == 'correct_account') {
+                return Promise.resolve({
+                    account,
+                    password: 'correct_password',
+                    id_: 'any_id'
+                })
+            }
 
-describe('login-validator', () => {
+            return Promise.resolve(null)
+        }
+    }
+    const fakeDbProvider = new FakeDbProvider()
+    const userRepository = new UserRepository(fakeDbProvider)
+    const validator = new LoginValidator()
+    const sut = new LoginController(validator, userRepository)
 
-    const sut = new LoginValidator()
-
-    it('should return 400 if there is no data', () => {
-        const result = sut.handle()
-        expect(result.statusCode).toBe(400)
+    it('Should 400 if there is no data', async () => {
+        let req = { body: {} }
+        let res = null
+        const { statusCode } = await sut.handle(req, res)
+        expect(statusCode).toBe(400)
     })
 
-    it('should return 400 if there is no account', () => {
-        let mockedUser = {
-            account: null,
-            password: 'test@test.com'
+    it('Should 400 if there is no account', async () => {
+        let req = {
+            body: {
+                account: 'no_exist_account',
+                password: 'password'
+            }
         }
-        const result = sut.handle(mockedUser)
-        expect(result.statusCode).toBe(400)
+        let res = null
+        const { statusCode, body } = await sut.handle(req, res)
+        expect(statusCode).toBe(400)
+        expect(body.message).toEqual('Account does not exists')
     })
-    
-    it('should return 400 if there is no password', () => {
-        let mockedUser = {
-            account: 'test',
-            password: null
+
+    it('Should 400 if wrong password is given', async () => {
+        let req = {
+            body: {
+                account: 'correct_account',
+                password: 'wrong_password'
+            }
         }
-        const result = sut.handle(mockedUser)
-        expect(result.statusCode).toBe(400)
+        let res = null
+        const { statusCode, body } = await sut.handle(req, res)
+        expect(statusCode).toBe(400)
+        expect(body.message).toEqual('Wrong credentials given')
     })
-    it('should return 200 if everything is correct', () => {
-        let mockedUser = {
-            account: 'test',
-            password: 'test@test.com'
+
+    it('Should 200 if login is ok', async () => {
+        let req = {
+            body: {
+                account: 'correct_account',
+                password: 'correct_password'
+            }
         }
-        const result = sut.handle(mockedUser)
-        expect(result.statusCode).toBe(200)
+        let res = null
+        const { statusCode, body } = await sut.handle(req, res)        
+        expect(statusCode).toBe(200)
+        expect(body).toEqual({
+            account: 'correct_account',
+            password: 'correct_password',
+            id_: 'any_id'
+        })
     })
 })
