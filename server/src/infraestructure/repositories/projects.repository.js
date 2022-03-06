e9a63a2d2154835a6470d9619a7f3f0146052c09
caio frias <caio.sousa@extreme.digital>
2022-03-06 08:37:34 -0300

    feat: add projects repository

---
+++ server/src/infraestructure/repositories/projects.repository.js
@@ -0,0 +1,19 @@
+const { ok } = require('../../interfaces/helpers/http-helpers')
+module.exports = class ProjectsRepository {
+
+    #dbProvider
+
+    constructor(dbProvider) {
+        this.#dbProvider = dbProvider
+    }
+
+    async findProjectsById(id) {
+        const hasProjects = await this.#dbProvider.findProjectsByUserId(id)
+        
+        if (!hasProjects) {
+            return ok({ message: 'No projects yet' })
+        }
+
+        return ok(hasProjects)
+    }
+}
