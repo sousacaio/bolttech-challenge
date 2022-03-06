cd0d6f07091e01a0cf8965bd47b4471526bd6af7
caio frias <caio.sousa@extreme.digital>
2022-03-06 09:58:58 -0300

    feat: create dashboard component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,12 @@
 import Wrapper from '../components/Wrapper'
 import { useParams } from "react-router-dom";
+import { useState } from 'react';
 const Dashboard = () => {
+
+    const [projects, setProjects] = useState([])
+
     let params = useParams()
+    
 
     return (
         <Wrapper fluid>
