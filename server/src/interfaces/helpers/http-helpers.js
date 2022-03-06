3aaea9cb30bd05fa8ca14e8f68e75be3cfa072a5
caio frias <caio.sousa@extreme.digital>
2022-03-06 08:10:03 -0300

    feat: add forbidden helper

--- server/src/interfaces/helpers/http-helpers.js
+++ server/src/interfaces/helpers/http-helpers.js
@@ -1,6 +1,9 @@
 exports.badRequest = (body) => {
     return { statusCode: 400, body }
 }
+exports.forbidden = (body) => {
+    return { statusCode: 403, body }
+}
 exports.ok = (body) => {
     return { statusCode: 200, body }
 }
