14b72ccf82ab03d843da77e6bb66c7b359bc0296
caio frias <caio.sousa@extreme.digital>
2022-03-06 08:38:09 -0300

    test: ensure findProjectsByUserId works

---
+++ server/tests/unit/projects-repository.test.js
@@ -0,0 +1,45 @@
+const ProjectsRepository = require("../../src/infraestructure/repositories/projects.repository")
+
+describe('Projects repository', () => {
+    class FakeDbProvider {
+        async findProjectsByUserId(id) {
+            if (id === 'id_with_activities') {
+                return Promise.resolve([{
+                    name: 'Project name',
+                    activities: [
+                        {
+                            id: 'activitie_id',
+                            name: 'activie 1',
+                            status: false
+                        }
+                    ]
+                }])
+            }
+            return Promise.resolve(null)
+        }
+    }
+
+    const fakeDbProvider = new FakeDbProvider()
+
+    const sut = new ProjectsRepository(fakeDbProvider)
+
+    it('Should projects data if user has projects', async () => {        
+        const { statusCode,body } = await sut.findProjectsById('id_with_activities')
+        expect(statusCode).toBe(200)        
+        expect(body[0].name).toBe('Project name')
+        expect(body[0].activities.length).toBe(1)
+    })
+
+    it('Should projects data if user has projects', async () => {    
+        const { statusCode,body } = await sut.findProjectsById('id_with_activities')
+        expect(statusCode).toBe(200)        
+        expect(body[0].name).toBe('Project name')
+        expect(body[0].activities.length).toBe(1)
+    })
+    
+    it('Should return no projects message if user has no projects yet', async () => {        
+        const { statusCode,body } = await sut.findProjectsById('any_id')
+        expect(statusCode).toBe(200)
+        expect(body.message).toBe('No projects yet')
+    })
+})
