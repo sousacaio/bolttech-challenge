540eab7daccc45e9be39fcff7589c199f4c2cfdc
caio frias <caio.sousa@extreme.digital>
2022-03-06 09:42:15 -0300

    feat: create projects route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router()
 const userRoutes = require('./user/index')
+const projecRoutes = require('./projects/index')
 
 routes.use('/user', userRoutes)
+routes.use('/projects', projecRoutes)
 
 module.exports = routes
